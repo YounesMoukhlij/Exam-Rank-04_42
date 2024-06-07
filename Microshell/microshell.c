73755c9fcda46d4295de590307fefbdbb2c39ceb
YounesMoukhlij <123767564+YounesMoukhlij@users.noreply.github.com>
2024-06-07 18:37:18 +0100

    new

--- Microshell/microshell.c
+++ Microshell/microshell.c
@@ -6,13 +6,34 @@
 /*   By: youmoukh <youmoukh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 17:06:28 by youmoukh          #+#    #+#             */
-/*   Updated: 2024/06/07 18:25:15 by youmoukh         ###   ########.fr       */
+/*   Updated: 2024/06/07 18:37:09 by youmoukh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+
+void	my_puts(char *str, int fd, int f_newline)
+{
+	int	i = 0x0;
+
+	while (str[i])
+		write(fd, &str[i++], 0x1);
+	if (f_newline == 0x1)
+		write(fd, "\n", 0x1);
+}
+
+int	cd(char *str, int num_arg)
+{
+	int	i = 0x0;
+
+	if (num_arg > 0x1)
+		return (my_puts("", 0x2, 0x1), 0x1);
+	i = chdir(str);
+	if (!i)
+		return (my_puts("", 0x2, 0x1), 0x1);
+}
 
 int	main(int ac, char **av, char **env)
 {
